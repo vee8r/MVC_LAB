77375cd09dd6e764fd845d44aedbbe4f65ee78de
Weronika Kuzio <w66347@student.wsiz.edu.pl>
2022-11-24 11:17:41 +0100

    lab7

---
+++ MVC_LAB/Controllers/PersonController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_LAB.Models.Person;
+
+namespace MVC_LAB.Controllers
+{
+    public class PersonController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            var model = new PersonViewModel()
+            {
+                Persons = new PersonModel().GetPersons()
+            };
+            return View(model);
+        }
+    }
+}
---
+++ MVC_LAB/Models/Person/PersonModel.cs
@@ -0,0 +1,53 @@
+﻿namespace MVC_LAB.Models.Person
+{
+    public class PersonModel
+    {
+        public PersonModel()
+        {
+
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public GenderEnum Gender { get; set; }
+        public string City { get; set; }
+
+        public List<PersonModel> GetPersons()
+        {
+            return new List<PersonModel>()
+            {
+                new PersonModel()
+                {
+                Name = "Andrzej",
+                City = "Rzeszow",
+                Gender = GenderEnum.Male,
+                ID = 1,
+                },
+                new PersonModel()
+                {
+                Name = "Katarzyna",
+                City = "Rzeszow",
+                Gender = GenderEnum.Female,
+                ID = 2,
+                },
+                new PersonModel()
+                {
+                Name ="Julia",
+                City = "Krakow",
+                Gender = GenderEnum.Female,
+                ID = 3,
+                },
+                new PersonModel()
+                {
+                Name = "Piotr",
+                City = " Warszawa",
+                Gender= GenderEnum.Male,
+                ID = 4,
+            },
+        };
+        }
+    }
+
+
+
+}
